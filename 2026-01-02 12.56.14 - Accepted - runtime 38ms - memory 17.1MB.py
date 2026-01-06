c25ab9dbc91de6fd9cff3ebff6ec114b5903d4ed
Pranay Mahendrakar <mahendrakarpranay@gmail.com>
2026-01-06 11:16:13 +0530

    Initial commit

---
+++ 2026-01-02 12.56.14 - Accepted - runtime 38ms - memory 17.1MB.py
@@ -0,0 +1,24 @@
+class Solution:
+    def strWithout3a3b(self, a: int, b: int) -> str:
+        result = []
+        
+        while a > 0 or b > 0:
+            # Check last two chars
+            if len(result) >= 2 and result[-1] == result[-2]:
+                # Need to add the other char
+                if result[-1] == 'a':
+                    result.append('b')
+                    b -= 1
+                else:
+                    result.append('a')
+                    a -= 1
+            else:
+                # Add char with larger count
+                if a >= b:
+                    result.append('a')
+                    a -= 1
+                else:
+                    result.append('b')
+                    b -= 1
+        
+        return ''.join(result)
